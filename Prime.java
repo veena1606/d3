48ba7625b6e1762128245fe2b3b388b0dd711d1a
veena1606 <rayapudiveenamadhuri16@gmail.com>
2024-11-27 22:47:52 +0530

    init

---
+++ Prime.java
@@ -0,0 +1,24 @@
+public class Prime{
+    public static void main(String[]args)
+    {
+        int n=10;
+        System.out.println("prime numbers");
+        for(int i=2;i<=n;i++)
+        {
+            boolean flag=true;
+        for(int j=2;j<=Math.sqrt(i);j++)
+        {
+            if(i%j==0)
+            {
+                flag=false;
+                break;
+            }
+        }
+        if(flag)
+        {
+            System.out.println(i);
+        }
+        }
+
+    }
+}
